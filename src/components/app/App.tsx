67f475cf1c873cb9351e991c8a51727ec38898d4
Wojciech Adaszyński <wojciech.adaszynski@gmail.com>
2018-02-15 16:49:29 +0100

    Description added

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -9,14 +9,16 @@ import './App.css'
 import {VariablesSelection} from "../settings/VariablesSelection";
 import {ParallelCoordChart} from "../parallel-coord-chart/ParallelCoordChart";
 import {AboutView} from "../about/AboutView";
+
 @observer
-export class App extends React.Component<{state: AppState}, {}> {
+export class App extends React.Component<{ state: AppState }, {}> {
     private onCountrySelect = (country: CountryModel) => {
         this.props.state.configuration.selectCountry(country);
     }
-    private onWaveSelect = (wave: Wave):void => {
+    private onWaveSelect = (wave: Wave): void => {
         this.props.state.loadWave(wave)
     }
+
     render() {
         return (
             <div className='container'>
@@ -26,12 +28,48 @@ export class App extends React.Component<{state: AppState}, {}> {
                 </header>
                 <hr/>
                 <div>
+                    <p>
+                        Every year The World Values Survey organization conducts a study about values, preferences and
+                        behaviours of people all around the globe. This interactive document visualizes changes that
+                        have happened in Europe during last 20 years using both WVS and Gapminder data.
+                    </p>
+                    <p>
+                        By clicking on “Politics important” button we can the average importance of politics in each
+                        country. We see that people from Hungary, Slovenia and Romania answered that politics are
+                        crucial for them.
+
+                    </p>
                     <Map configuration={this.props.state.configuration}
                          countriesWithStatisticsCodes={this.props.state.countriesWithStatisticsCodes}
                          countries={this.props.state.countries}/>
-                    <VariablesSelection  configuration={this.props.state.configuration}
-                                         onWaveChange={this.onWaveSelect}
-                                         variables={this.props.state.configuration.allVariables}/>
+                    <VariablesSelection configuration={this.props.state.configuration}
+                                        onWaveChange={this.onWaveSelect}
+                                        variables={this.props.state.configuration.allVariables}/>
+
+                    <p>
+                        Let’s see if high interest in politics correlate with the national pride. While holding Ctrl key
+                        click on those countries. Parallel coordinates chart should highlight lines corresponding to
+                        these countries. We can see that there’s no direct correlation between those two variables.
+                    </p>
+                    <p>
+
+                    Do you wanna see the most dense areas in Europe? Click “Population Density” button. If you are not
+                    sure about some country, click on it and check its name on the bottom of the page.
+                    </p>
+                    <p>
+
+                        Now, let’s inspect the average life expectancy in Europe. Click on the “Life expectancy” button.
+                        It is clear that people live longer (on average) in the western parts of Europe. With the help
+                        of time slider we can see how the metric has during last 20 years. Slide the slider to the right
+                        to see the progress of life expectancy in Europe.
+                    </p>
+                    <p>
+
+                        If you want to check the detailed information about some country, click on the map (without Ctrl
+                        key). At the bottom of the page you will see the current population, currency and flag of the
+                        selected country.
+
+                    </p>
 
                 </div>
                 <div>
@@ -43,11 +81,11 @@ export class App extends React.Component<{state: AppState}, {}> {
                     />
                 </div>
                 <div>
-                    <CountryDetails configuration={this.props.state.configuration} />
+                    <CountryDetails configuration={this.props.state.configuration}/>
 
                 </div>
-                <AboutView />
-                <DevTools />
+                <AboutView/>
+                <DevTools/>
             </div>
         );
     }
